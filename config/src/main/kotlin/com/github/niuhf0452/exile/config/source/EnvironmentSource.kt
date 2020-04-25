244e37127be208112ba1bb89432fc73f4fd6f267
haifeng.niu <niuhf0452@gmail.com>
2020-04-25 11:16:52 +0800

    Fix environment variable issue.

--- config/src/main/kotlin/com/github/niuhf0452/exile/config/source/EnvironmentSource.kt
+++ config/src/main/kotlin/com/github/niuhf0452/exile/config/source/EnvironmentSource.kt
@@ -11,8 +11,13 @@ import java.net.URI
 class EnvironmentSource : Config.Source {
     override fun load(): Iterable<ConfigValue> {
         log.info("Load config from environment variables")
-        return System.getenv().map { (k, v) ->
-            ConfigValue(location, k, v)
+        return System.getenv().mapNotNull { (k, v) ->
+            if (v.contains('$')) {
+                log.debug("Ignore environment variable, because it contains expression: $k")
+                null
+            } else {
+                ConfigValue(location, k, v)
+            }
         }
     }
 
